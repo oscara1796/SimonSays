6e2e0a19ad9dbb2be0a505c9d94b794ba0e8c5e9
Abraham <oscara1706cl@gmail.com>
2019-09-16 16:27:24 -0500

    opciones de elegir color resulatas

--- simonDice.js
+++ simonDice.js
@@ -4,19 +4,21 @@ const violeta = document.getElementById('violeta');
 const naranja = document.getElementById('naranja');
 const verde = document.getElementById('verde');
 const btnEmpezar = document.getElementById('btnEmpezar');
+const ULTIMO_NIVEL = 10;
 
 
 class Juego{
   constructor(){
     this.inicializar();
     this.generarSecuencia();
-    this.siguienteNivel();
+    setTimeout(this.siguienteNivel, 500)
   }
 
   inicializar(){
+    this.siguienteNivel = this.siguienteNivel.bind(this)
     this.elegirColor = this.elegirColor.bind(this)
     btnEmpezar.classList.add('hide')
-    this.nivel=10
+    this.nivel=1
     this.colores ={
       celeste,
       violeta,
@@ -29,11 +31,12 @@ class Juego{
     // de esta forma con new arrays con el metodo fill se llena el array con 0
     //con Math floor se redondea un decimal
     //Math.random() solo genera un decimal entre el 0 y 1
-    this.secuencia = new Array(10).fill(0).map(n => Math.floor(Math.random()*4))
+    this.secuencia = new Array(ULTIMO_NIVEL).fill(0).map(n => Math.floor(Math.random()*4))
   }
   siguienteNivel(){
+    this.subNivel = 0
     this.iluminarSecuencia()
-    this.agregarEventosClick()
+
   }
   asignarAnumeroColor(num){
     switch (num) {
@@ -47,19 +50,34 @@ class Juego{
         return 'verde'
     }
   }
+  asignarAcolorNumero(color){
+    switch (color) {
+      case 'celeste':
+        return 0
+      case 'violeta':
+        return 1
+      case 'naranja':
+        return 2
+      case 'verde':
+        return 3
+    }
+  }
   iluminarSecuencia(){
     for (let i = 0; i < this.nivel; i++) {
       const color = this.asignarAnumeroColor(this.secuencia[i])
 
-      setTimeout(() => this.iluminarColor(color), 1000 * i)
+      setTimeout(() => this.iluminarColor(color,i), 1000 * i)
     }
   }
-  iluminarColor(color){
+  iluminarColor(color,i){
     this.colores[color].classList.add('light')
-    setTimeout(() =>this.apagarColor(color),350)
+    setTimeout(() =>this.apagarColor(color,i),350)
   }
-  apagarColor(color){
+  apagarColor(color,i){
     this.colores[color].classList.remove('light')
+    if ((i+1) === this.nivel) {
+        this.agregarEventosClick()
+    }
   }
   agregarEventosClick(){
     // con el metodo bind enlacemos el this del evento y lo convertimos al this del juego u objeto
@@ -68,10 +86,37 @@ class Juego{
     this.colores.naranja.addEventListener('click', this.elegirColor)
     this.colores.verde.addEventListener('click', this.elegirColor)
   }
-  elegirColor(ev){
-    // En un evento this es el objeto html al que se le escucha el evento
-    console.log(this);
+
+  eliminarEventosClick(){
+    this.colores.celeste.removeEventListener('click', this.elegirColor)
+    this.colores.violeta.removeEventListener('click', this.elegirColor)
+    this.colores.naranja.removeEventListener('click', this.elegirColor)
+    this.colores.verde.removeEventListener('click', this.elegirColor)
   }
+  elegirColor(ev){
+
+    // En un evento this es el objeto html al que se le escucha el evento
+    const nombreColor = ev.target.dataset.color
+    const numeroColor = this.asignarAcolorNumero(nombreColor)
+    this.iluminarColor(nombreColor)
+      // console.log(numeroColor);
+    if(numeroColor === this.secuencia[this.subNivel]){
+      this.subNivel++
+      console.log(this.subNivel);
+      if (this.subNivel === this.nivel) {
+        this.nivel++;
+
+        this.eliminarEventosClick();
+        if (this.nivel === (ULTIMO_NIVEL+1)) {
+          //Gano!
+        }else{
+          setTimeout(this.siguienteNivel, 1500)
+        }
+      }
+    }else{
+      //Perdió
+    }
+}
 }
 
 function empezarJuego(){
